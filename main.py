f6cae3a6dec1a9182c50a37a7437585a8505eb43
YouAreThere <50164237+YouAreThere@users.noreply.github.com>
2021-06-13 13:17:36 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from spiking_model import*
 #from visdom import Visdom
 # os.environ['CUDA_VISIBLE_DEVICES'] = "3"
 names = 'spiking_model'
-data_path =  './raw/' #todo: input your data path
-device = torch.device("cpu")
-train_dataset = torchvision.datasets.MNIST(root=data_path, train=True, download=True, transform=transforms.ToTensor())
+data_path =  './cifar10/' #todo: input your data path
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_dataset = torchvision.datasets.CIFAR10(root=data_path, train=True, download=True, transform=transforms.ToTensor())
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
-
-test_set = torchvision.datasets.MNIST(root= data_path, train=False, download=True,  transform=transforms.ToTensor())
+#transform.ToTensor: [0,255]numpy array->[0,1]tensor
+test_set = torchvision.datasets.CIFAR10(root= data_path, train=False, download=True,  transform=transforms.ToTensor())
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0)
 
 best_acc = 0  # best test accuracy
@@ -20,25 +20,27 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 acc_record = list([])
 loss_train_record = list([])
 loss_test_record = list([])
-checkpoint = torch.load('./checkpoint/3prune_ckptspiking_model.t7')
+checkpoint = torch.load('./checkpoint/4ckptspiking_model.t7')
 snn = SCNN()
 snn.load_state_dict(checkpoint['net'])
 snn.to(device)
-
+#define loss function and optimizer
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(snn.parameters(), lr=learning_rate)
 global_step = 0
+#train network
 for epoch in range(num_epochs):
     running_loss = 0
     start_time = time.time()
     for i, (images, labels) in enumerate(train_loader):
         snn.zero_grad()
         optimizer.zero_grad()
-
         images = images.float().to(device)
-
         outputs = snn(images)
         labels_ = torch.zeros(batch_size, 10).scatter_(1, labels.view(-1, 1), 1)
+        # print(labels)
+        # print(labels_)
+        # print("########\n",outputs)
         loss = criterion(outputs.cpu(), labels_)
         running_loss += loss.item()
         loss.backward()
@@ -49,9 +51,15 @@ for epoch in range(num_epochs):
                     %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size,running_loss ))
              running_loss = 0
              print('Time elasped:', time.time()-start_time)
+        # if (i+1) %1000 == 0:
+        #         print('Saving...')
+        #         state = {
+        #             'net': snn.state_dict()
+        #         }
+        #         torch.save(state, './checkpoint/4ckpt'+names+'.t7')
     correct = 0
     total = 0
-    optimizer = lr_scheduler(optimizer, epoch, learning_rate, 40)
+    optimizer = lr_scheduler(optimizer, epoch, learning_rate, 5)
 
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(test_loader):
@@ -66,12 +74,12 @@ for epoch in range(num_epochs):
             if batch_idx %100 ==0:
                 acc = 100. * float(correct) / float(total)
                 print(batch_idx, len(test_loader),' Acc: %.5f' % acc)
-    
+            
     print('Iters:', epoch,'\n\n\n')
     print('Test Accuracy of the model on the 10000 test images: %.3f' % (100 * correct / total))
     acc = 100. * float(correct) / float(total)
     acc_record.append(acc)
-    if epoch % 5 == 0:
+    if epoch % 1 == 0:
         print(acc)
         print('Saving..')
         state = {
--- spiking_model.py
+++ spiking_model.py
@@ -7,12 +7,12 @@ import numpy as np
 device = torch.device( "cpu")
 thresh = 0.5 # neuronal threshold
 lens = 0.5 # hyper-parameters of approximate function
-decay = 0.25 # decay constants
+decay = 0.6 # decay constants
 num_classes = 10
-batch_size  = 100
-learning_rate = 1e-3
-num_epochs = 5 # max epoch
-aux_decay = 0.25 # decay constant for auxiliary neurons
+batch_size  = 16
+learning_rate = 1e-4
+num_epochs = 20 # max epoch
+#aux_decay = 0.25 # decay constant for auxiliary neurons
 
 # define approximate firing function
 class ActFun(torch.autograd.Function):
@@ -32,32 +32,33 @@ act_fun = ActFun.apply
 # membrane potential update
 
 def mem_update(ops, aux_ops, x, mem, spike):
-    mem = mem * decay * (1. - spike) + ops(x) - aux_ops(x) #待修改！！
+    tmp = ops(x) - 0.1*aux_ops(x)
+    tmp = torch.relu(tmp)
+    mem = mem * decay * (1. - spike) + tmp
     spike = act_fun(mem) # act_fun : approximation firing function
     return mem, spike
-
-# auxiliary neurons membrane potential update/待修改！不一定管用
-
 
 def original_mem_update(ops, x, mem, spike): #无辅助层情况下的膜电位更新，仅输出层使用
     mem = mem * decay * (1. - spike) + ops(x)
     spike = act_fun(mem) # act_fun : approximation firing function
     return mem, spike
 # cnn_layer(in_planes, out_planes, stride, padding, kernel_size)
-cfg_cnn = [(1, 8, 1, 1, 3),
-           (8, 8, 1, 1, 3),]
+cfg_cnn = [(3, 32, 1, 1, 3),
+           (32, 64, 1, 1, 3),
+           (64, 32, 1, 1, 3),
+           (32, 1, 1, 0, 1)]
 # kernel size
-cfg_kernel = [28, 14, 7]
+cfg_kernel = [32, 16, 8, 8]
 # fc layer
-cfg_fc = [128, 10]
+cfg_fc = [10]
 
 # Dacay learning_rate
 
 def lr_scheduler(optimizer, epoch, init_lr=0.1, lr_decay_epoch=50):
-    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    """Decay learning rate by a factor of 0.5 every lr_decay_epoch epochs."""
     if epoch % lr_decay_epoch == 0 and epoch > 1:
         for param_group in optimizer.param_groups:
-            param_group['lr'] = param_group['lr'] * 0.1
+            param_group['lr'] = param_group['lr'] * 0.5
     return optimizer
 
 class SCNN(nn.Module):
@@ -69,41 +70,64 @@ class SCNN(nn.Module):
         in_planes, out_planes, stride, padding, kernel_size = cfg_cnn[1]
         self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
         self.aux2 = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
-
-        self.fc1 = nn.Linear(cfg_kernel[-1] * cfg_kernel[-1] * cfg_cnn[-1][1], cfg_fc[0], bias=False)
-        self.aux3 = nn.Linear(cfg_kernel[-1] * cfg_kernel[-1] * cfg_cnn[-1][1], cfg_fc[0], bias=False)
-        self.fc2 = nn.Linear(cfg_fc[0], cfg_fc[1], bias=False)
+        in_planes, out_planes, stride, padding, kernel_size = cfg_cnn[2]
+        self.conv3 = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.aux3 = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        in_planes, out_planes, stride, padding, kernel_size = cfg_cnn[3]
+        self.conv4 = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.aux4 = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.fc = nn.Linear(cfg_cnn[-1][1]*cfg_kernel[-1]*cfg_kernel[-1], cfg_fc[0], bias=False)
  
     def forward(self, input, time_window = 20):
-        c1_mem = c1_spike = torch.zeros(batch_size, cfg_cnn[0][1], cfg_kernel[0], cfg_kernel[0], device=device)
-        c2_mem = c2_spike = torch.zeros(batch_size, cfg_cnn[1][1], cfg_kernel[1], cfg_kernel[1], device=device)
-
-        h1_mem = h1_spike = h1_sumspike = torch.zeros(batch_size, cfg_fc[0], device=device)
-        h2_mem = h2_spike = h2_sumspike = torch.zeros(batch_size, cfg_fc[1], device=device)
-
+        c1_mem = 0.5*torch.ones(batch_size, cfg_cnn[0][1], cfg_kernel[0], cfg_kernel[0], device=device)
+        c1_spike = torch.zeros(batch_size, cfg_cnn[0][1], cfg_kernel[0], cfg_kernel[0], device=device)
+        c2_mem = 0.5*torch.ones(batch_size, cfg_cnn[1][1], cfg_kernel[1], cfg_kernel[1], device=device)
+        c2_spike = torch.zeros(batch_size, cfg_cnn[1][1], cfg_kernel[1], cfg_kernel[1], device=device)
+        #c3_mem = c3_spike = torch.zeros(batch_size, cfg_cnn[2][1], cfg_kernel[2], cfg_kernel[2], device=device)
+        c3_spike = torch.zeros(batch_size, cfg_cnn[2][1], cfg_kernel[2], cfg_kernel[2], device=device)
+        c3_mem = 0.5*torch.ones(batch_size, cfg_cnn[2][1], cfg_kernel[2], cfg_kernel[2], device=device)
+        c4_spike = torch.zeros(batch_size, cfg_cnn[3][1], cfg_kernel[2], cfg_kernel[2], device=device)
+        c4_mem = 0.5*torch.ones(batch_size, cfg_cnn[3][1], cfg_kernel[2], cfg_kernel[2], device=device)
+        h_spike = h_sumspike = torch.zeros(batch_size, cfg_fc[0], device=device)
+        h_mem = 0.5*torch.ones(batch_size, cfg_fc[0], device=device)
+        '''
+        fid1 = open(file='D:/vscode/SNN_with_pruning/conv1_mem.txt',mode='w')
+        fid2 = open(file='D:/vscode/SNN_with_pruning/conv1_spike.txt',mode='w')
+        fid3 = open(file='D:/vscode/SNN_with_pruning/conv2_mem.txt',mode='w')
+        fid4 = open(file='D:/vscode/SNN_with_pruning/conv2_spike.txt',mode='w')
+        '''
         for step in range(time_window): # simulation time steps
-
+            #encode the input
             x = input > torch.rand(input.size(), device=device) # prob. firing
-            tmp = x
             
-
             c1_mem, c1_spike = mem_update(self.conv1, self.aux1, x.float(), c1_mem, c1_spike)
-
+            '''
+            print('step: %d'%step, file=fid1)
+            print('step: %d'%step, file=fid2)
+            print('step: %d'%step, file=fid3)
+            print('step: %d'%step, file=fid4)
+            print(c1_mem.detach().numpy(),file=fid1)
+            print(c1_spike.detach().numpy(),file=fid2)
+            '''
             x = F.max_pool2d(c1_spike, 2)
-
-            tmp = x
-
+            
             c2_mem, c2_spike = mem_update(self.conv2, self.aux2, x, c2_mem,c2_spike)
-
+            '''
+            print(c2_mem.detach().numpy(),file=fid3)
+            print(c2_spike.detach().numpy(),file=fid4)
+            '''
+            
             x = F.max_pool2d(c2_spike, 2)
+            
+            c3_mem, c3_spike = mem_update(self.conv3, self.aux3, x, c3_mem, c3_spike)
+            c4_mem, c4_spike = mem_update(self.conv4, self.aux4, c3_spike, c4_mem, c4_spike)
+            x = c4_spike
             x = x.view(batch_size, -1)
 
-            #待修改！！
-            tmp = x
-            h1_mem, h1_spike = mem_update(self.fc1, self.aux3, x, h1_mem, h1_spike)
-            h1_sumspike += h1_spike
-            h2_mem, h2_spike = original_mem_update(self.fc2, h1_spike, h2_mem,h2_spike)
-            h2_sumspike += h2_spike
+           
+            
+            h_mem, h_spike = original_mem_update(self.fc, x, h_mem,h_spike)
+            h_sumspike += h_spike
             """
             for i in [10]:
                 emit1_single = h1_spike[i].numpy()[0]
@@ -122,7 +146,13 @@ class SCNN(nn.Module):
                 viz.scatter(emit1_plt,opts={'markersize':2, 'title':'fc_spike','layoutopts': {'plotly': {'xaxis': { 'range': [1, 128],'autorange': False},'yaxis': {'range': [0, 20],'autorange': False}}}},win='h1_spike',update='append')
                 viz.scatter(emit2_plt,opts={'markersize':2, 'title':'output_spike','layoutopts': {'plotly': {'xaxis': { 'range': [1, 10],'autorange': False},'yaxis': {'range': [0, 20],'autorange': False}}}},win='h2_spike',update='append')
         """
-        outputs = h2_sumspike / time_window
+        outputs = h_sumspike / time_window
+        '''
+        fid1.close()
+        fid2.close()
+        fid3.close()
+        fid4.close()
+        '''
         """
         clearset = torch.zeros(1,2)  
         viz.scatter(clearset,opts={'markersize':2, 'title':'fc1_spike generated by the 11th image','layoutopts': {'plotly': {'xaxis': { 'range': [1, 128],'autorange': False},'yaxis': {'range': [0, 20],'autorange': False}}}},win='h1_spike')
